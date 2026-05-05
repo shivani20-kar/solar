da404e50a3bb04e7a50056570767055a6106ec35
solar <shivanikarhale40@gmail.com>
2026-05-05 12:19:06 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-  Hello
-  
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
