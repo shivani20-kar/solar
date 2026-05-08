bb82fd42aac123e507b4154d68e7e86e724eccc3
Pratiksha Kate <katepratiksha09@gmail.com>
2026-05-08 12:44:01 +0530

    herodone

--- src/HomePage/HeroSection.jsx
+++ src/HomePage/HeroSection.jsx
@@ -3,53 +3,57 @@ import "./CSs/HeroSection.css";
 import heroImg from "./Images/Gradient.png";
 
 const HeroSection = () => {
-  return (
-    <section className="solar-hero">
-      <img src={heroImg} alt="Solar Hero" className="hero-bg" />
+    return (
+        <section className="solar-hero">
+            <img src={heroImg} alt="Solar Hero" className="hero-bg" />
 
-      <div className="hero-overlay"></div>
+            <div className="hero-overlay"></div>
 
-      <div className="hero-content">
-        <span className="hero-tag">✦ PREMIUM SOLAR PANELS</span>
+            <div className="hero-content">
+                <span className="hero-tag">✦ PREMIUM SOLAR PANELS</span>
 
-        <h1>
-          Empowering <br />
-          Homes with the <br />
-          Power of the Sun
-        </h1>
+                <h1>
+                    Empowering <br />
+                    Homes with the <br />
+                    Power of the Sun
+                </h1>
 
-        <p>
-          Imagine a world where your energy is clean, affordable, and limitless.
-          With our cutting-edge solar solutions, you can harness the power of
-          the sun to reduce.
-        </p>
+                <p>
+                    Imagine a world where your energy is clean, affordable, and limitless.
+                    With our cutting-edge solar solutions, you can harness the power of
+                    the sun to reduce.
+                </p>
 
-        <button className="hero-btn">
-          Get Started
-          <span>↗</span>
-        </button>
-      </div>
+                <button className="hero-btn">
+                    Get Started
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                        <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+                    </svg>
+                </button>
+            </div>
+{/* 
+            <div className="hero-card">
+                <div className="stat-box-hero small-card">
+                    <h2>120+</h2>
+                    <p>Expert Solution</p>
+                </div>
 
-      <div className="hero-card">
-        <div className="card-top">
-          <p>Join as Dealership</p>
-          <button>↗</button>
-        </div>
+                <div className="stat-box-hero big-card">
+                    <div className="card-top-hero">
+                        <p>Join as Dealership</p>
+                        <button><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                            <path d="M647-440H160v-80h487L423-744l57-56 320 320-320 320-57-56 224-224Z" />
+                        </svg></button>
+                    </div>
 
-        <div className="card-stats">
-          <div className="stat-box">
-            <h2>120+</h2>
-            <p>Expert Solution</p>
-          </div>
+                    <h2>250+</h2>
+                    <p>Enterprises to thrive</p>
+                </div>
 
-          <div className="stat-box">
-            <h2>250+</h2>
-            <p>Enterprises to thrive</p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+
+            </div> */}
+        </section>
+    );
 };
 
 export default HeroSection;
